c7b944bf1bc5fea3b8f465ef016c9ce803102338
winoz <winoz@naver.com>
2023-11-02 23:47:03 +0900

    tes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
+import { useState } from 'react'; 
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let [a,b] = useState(0);
+  let [modal,setModal] = useState(false)
+  let [myData,setData] = useState(
+    ["안녕하세요","만나서반갑네요","고맙습니다."]
+    )
+  function han(){
+    b(a+1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{a}</div>
+     <button onClick={han}>test</button>
+     <button onClick={()=>{setModal(true)}}>열기</button>
+
+     {
+        // myData.map(function(a,i){
+        //   return(
+        //     <div>{myData[i]}</div>
+        //   )
+        // })
+
+       myData.map(function(a,i){
+        return(
+          <div key={i}>{a}</div>
+        )
+       })
+     }
+    
+     {
+      modal==true ? <Modal props={()=>setModal(false)}></Modal>:null
+     }
+     
+
+
+
     </div>
   );
 }
+
+function Modal({props}){
+  return(
+    <div className="modal" >
+      <h2>제목</h2>
+      <p>test</p>
+      <button onClick={props}>닫기</button>
+    </div>
+
+
+  )
+}
+
+
 
 export default App;
